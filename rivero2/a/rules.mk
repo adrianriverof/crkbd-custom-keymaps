8ea21b608ad47628d1c36eb7bade6bd994e6e9ba
adrianriverof <adrianriverof@gmail.com>
2024-04-20 11:14:23 +0200

    build: delete many things
    delete all the different versions, we only support the main, based on rivero2 keymap
    Maybe we would make different versions with oled options in the future

--- rivero2/a/rules.mk
+++
@@ -1,9 +0,0 @@
-RGBLIGHT_ENABLE    = no
-RGB_MATRIX_ENABLE  = yes
-MOUSEKEY_ENABLE    = no
-NKRO_ENABLE        = yes
-OLED_ENABLE        = yes
-OLED_DRIVER        = SSD1306
-LTO_ENABLE         = yes
-LOCAL_GLCDFONT = yes
-
--- rivero3/rules.mk
+++
@@ -1,6 +0,0 @@
-RGBLIGHT_ENABLE    = no
-RGB_MATRIX_ENABLE  = yes
-MOUSEKEY_ENABLE    = yes
-NKRO_ENABLE        = yes
-OLED_ENABLE = yes
-LOCAL_GLCDFONT = yes

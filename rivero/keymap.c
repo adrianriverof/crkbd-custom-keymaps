26b6c8c6c07720dd1f0e913ab37d435b278cda5a
adrianriverof <adrianriverof@gmail.com>
2024-04-20 11:35:27 +0200

    feat: eliminate the use of Bloq_MAYUS
    hopefully I will be at peace now

--- rivero/keymap.c
+++ rivero/keymap.c
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,---------------------------------------------.
      KC_TAB,  KC_DOT,  KC_COMM,  KC_SCLN,  KC_P, KC_Y,            KC_F,  KC_G,  KC_C,  KC_H,  KC_L,  KC_BSPC,
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-   LCTL_T(KC_CAPS),  KC_A,  KC_O,  KC_E,  KC_U,  KC_I,                    KC_D,  KC_R,  KC_T,  KC_N, KC_S,KC_QUOT,
+   KC_LCTL,  KC_A,  KC_O,  KC_E,  KC_U,  KC_I,                    KC_D,  KC_R,  KC_T,  KC_N, KC_S,KC_QUOT,
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
     KC_LALT,  KC_SLSH,  KC_Q,  KC_J,  KC_K,  KC_X,           KC_B,  KC_M,KC_W,KC_V,KC_Z,LSFT_T(KC_ENT),
   //|------+------+------+------+------+------+------|  |------+------+------+-------+------+-------+--------|
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     //,-----------------------------------------.                ,---------------------------------------------. 
       KC_TAB,  KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,                   KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_BSPC,
     //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-      LCTL_T(KC_CAPS),  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                  KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,
+      KC_LCTL,  KC_A,  KC_S,  KC_D,  KC_F,  KC_G,                  KC_H,  KC_J,  KC_K,  KC_L, KC_SCLN,KC_QUOT,
     //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
       KC_LALT,  KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,                KC_N,  KC_M,KC_COMM,KC_DOT,KC_SLSH,LSFT_T(KC_ENT),
     //|------+------+------+------+------+------+------|  |------+------+------+-------+------+-------+--------|
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------.                ,---------------------------------------------.
      KC_TAB,  KC_DOT,  KC_COMM,  KC_SCLN,  KC_P, KC_Y,            KC_F,  KC_G,  KC_C,  KC_H,  KC_L,  KC_BSPC,
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
-   LCTL_T(KC_CAPS),  KC_A,  KC_O,  KC_E,  KC_U,  KC_I,                    KC_D,  KC_R,  KC_T,  KC_N, KC_S,KC_QUOT,
+   KC_LCTL,  KC_A,  KC_O,  KC_E,  KC_U,  KC_I,                    KC_D,  KC_R,  KC_T,  KC_N, KC_S,KC_QUOT,
   //|------+------+------+------+------+------|                |------+------+-------+------+-------+--------|
     KC_LALT,  KC_SLSH,  KC_Q,  KC_J,  KC_K,  KC_X,           KC_B,  KC_M,KC_W,KC_V,KC_Z,LSFT_T(KC_ENT),
   //|------+------+------+------+------+------+------|  |------+------+------+-------+------+-------+--------|

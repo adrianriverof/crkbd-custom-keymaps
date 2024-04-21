3c1ef0f545987c0423d3058f7daa7d7d852b887a
adrianriverof <adrianriverof@gmail.com>
2024-04-21 07:20:12 +0200

    feat: mouse momentary accel

--- rivero/config.h
+++ rivero/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Select hand configuration */
 
-#define MK_3_SPEED
+#define MK_MOMENTARY_ACCEL
 //#define MK_KINETIC_SPEED
 
 #define MASTER_RIGHT

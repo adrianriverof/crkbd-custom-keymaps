8b4f0ed54424d191dbbcf37d8e5c2624af67b93a
adrianriverof <adrianriverof@gmail.com>
2024-04-21 07:25:11 +0200

    feat: toggle mouse with x

--- rivero/keymap.c
+++ rivero/keymap.c
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
     KC_BSPC, LCTL(KC_Y), KC_4,   KC_5,   KC_6,   KC_0,                KC_MPLY,KC_LEFT,KC_DOWN,KC_RIGHT,KC_MS_U,KC_BTN2,
   //|------+------+-------+-------+-------+-------|                |------+------+------+------+------+------|
-    KC_ENT, LCTL(KC_Z), KC_1, KC_2,  KC_3,  KC_BTN1,                    KC_NO, KC_NO, KC_BTN1, KC_MS_L,  KC_MS_D, KC_MS_R,
+    KC_ENT, LCTL(KC_Z), KC_1, KC_2,  KC_3,  TG(_MOUSE),                    KC_NO, KC_NO, KC_BTN1, KC_MS_L,  KC_MS_D, KC_MS_R,
   //|------+------+-------+-------+-------+-------+------|  |------+------+------+------+------+------+------|
                                     TG(_MOUSE), LOWER,KC_SPC,   KC_ENT, RAISE,KC_LCTL
                                   //`--------------------'  `--------------------'
